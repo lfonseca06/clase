afc6887241ebf0b0b4381ed517711756d60067b5
lfonseca06 <167661509+lfonseca06@users.noreply.github.com>
2026-03-10 03:14:32 +0000

    Actualización de la API

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 #Definir al menos un endpoint
 @app.get("/")
 def home():
-    return {"mensaje": "Mi API esta funcionando"}
+    return {"mensaje": "Mi API esta funcionando y nuevo mensaje"}
